2829a036c20a95079364e0fa146ceda129a6c4e9
Filipe <filiperivelli@gmail.com>
2021-10-20 11:16:12 -0300

    Manutencao nos novos ambientes

--- split_bills/settings/base.py
+++ split_bills/settings/base.py
@@ -98,7 +98,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 
 LOGIN_REDIRECT_URL = 'house-list'
 LOGIN_URL = 'login'
