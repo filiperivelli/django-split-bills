f7be4f35f6a62c7cf83e09f00e8d302ce66d7320
Filipe Rivelli <filiperivelli@MacBook-Pro-de-Filipe.local>
2021-10-24 17:01:22 -0300

    Alteracoes no congi

--- split_bills/settings/__init__.py
+++ split_bills/settings/__init__.py
@@ -2,9 +2,9 @@ from .base import *
 import os
 import django_heroku
 
-if os.environ.get("ENV_NAME") == 'production':
+if os.environ['ENV_NAME'] == 'production':
     from .production import *
-elif os.environ.get("ENV_NAME") == 'local':
+elif os.environ['ENV_NAME'] == 'local':
     from .local import *
 else:
     from .local import *
